32c1094f2e0023c0e8810ebc354d790a4e431e12
Diana Yelemes <yelemessovaaaa@gmail.com>
2023-12-05 16:22:41 +0600

    2

--- models/models.go
+++ models/models.go
@@ -1,6 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type Habit struct {
+	gorm.Model
 	ID                  uint   `json:"id"`
 	Habit_Name          string `json:"habit_name"`
 	Target_Repeat_Count uint   `json:"target_repeat_count"`
